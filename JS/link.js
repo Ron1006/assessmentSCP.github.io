092762181c8fb7a5e8e5fb27226667f8905eddaa
Ron1006 <128130527+Ron1006@users.noreply.github.com>
2023-08-23 19:07:19 +1200

    Add files via upload

---
+++ JS/link.js
@@ -0,0 +1,19 @@
+document.getElementById('scp002').addEventListener('click', function() {
+    window.location.href = 'SCP002.html';
+});
+
+document.getElementById('scp003').addEventListener('click', function() {
+    window.location.href = 'SCP003.html';
+});
+
+document.getElementById('scp004').addEventListener('click', function() {
+    window.location.href = 'SCP004.html';
+});
+
+document.getElementById('scp005').addEventListener('click', function() {
+    window.location.href = 'SCP005.html';
+});
+
+document.getElementById('scp006').addEventListener('click', function() {
+    window.location.href = 'SCP006.html';
+});
